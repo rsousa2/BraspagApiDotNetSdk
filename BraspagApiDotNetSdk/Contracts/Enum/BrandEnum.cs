11b484c37b8e47def0e6e4e19e2ef04262d3ef92
Diogo Mello <diogoveras@gmail.com>
2017-01-04 16:14:34 -0200

    Atualização das bandeiras suportadas (#5)
    * Atualização do NuGet.exe
    
    * Atualização das bandeiras suportadas
    
    * Atualização das informações referentes ao pacote NuGet

--- BraspagApiDotNetSdk/Contracts/Enum/BrandEnum.cs
+++ BraspagApiDotNetSdk/Contracts/Enum/BrandEnum.cs
@@ -1,6 +1,6 @@
 ﻿namespace BraspagApiDotNetSdk.Contracts.Enum
 {
-	public enum BrandEnum
+    public enum BrandEnum
 	{
 		Undefined = 0,
 		Visa = 1,
@@ -11,5 +11,13 @@
 		Jcb = 6,
 		Diners = 7,
 		Discover = 8,
-	}
+        Hipercard = 9,
+        Hiper = 10,
+        Naranja = 11,
+        Nevada = 12,
+        Cabal = 13,
+        Credz = 14,
+        CredSystem = 15,
+        Banese = 16
+    }
 }
