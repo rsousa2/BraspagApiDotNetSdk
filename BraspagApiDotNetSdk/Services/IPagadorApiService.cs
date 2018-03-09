6b4fcc5780718a40e9d7118c4ab02ccd398253e5
Luiz Augusto Frambach <frambachluiz@gmail.com>
2018-03-09 17:30:24 -0300

    Correção , inclusao da assinatura do metodo na interface

--- BraspagApiDotNetSdk/Services/IPagadorApiService.cs
+++ BraspagApiDotNetSdk/Services/IPagadorApiService.cs
@@ -1,16 +1,19 @@
 ﻿using BraspagApiDotNetSdk.Contracts;
 using System;
+using System.Collections.Generic;
 
 namespace BraspagApiDotNetSdk.Services
 {
-	public interface IPagadorApiService
-	{
-		Sale CreateSale(MerchantAuthentication merchantAuthentication, Sale sale);
+    public interface IPagadorApiService
+    {
+        Sale CreateSale(MerchantAuthentication merchantAuthentication, Sale sale);
 
-		CaptureResponse Capture(Guid paymentId, MerchantAuthentication merchantAuthentication, CaptureRequest captureRequest);
+        CaptureResponse Capture(Guid paymentId, MerchantAuthentication merchantAuthentication, CaptureRequest captureRequest);
 
-		VoidResponse Void(Guid paymentId, MerchantAuthentication merchantAuthentication, VoidRequest voidRequest);
+        VoidResponse Void(Guid paymentId, MerchantAuthentication merchantAuthentication, VoidRequest voidRequest);
 
-		Sale Get(Guid paymentId, MerchantAuthentication merchantAuthentication);
-	}
+        Sale Get(Guid paymentId, MerchantAuthentication merchantAuthentication);
+
+        Sale CreateSale(MerchantAuthentication merchantAuthentication, Sale sale, Dictionary<string, string> headers);
+    }
 }
